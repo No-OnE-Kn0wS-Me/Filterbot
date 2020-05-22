d170fe69a7966139ea047da7bf6ea904bd9ef052
സൽമാൻ <61979182+salmanpdr@users.noreply.github.com>
2020-05-22 16:47:30 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -19,7 +19,7 @@ from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
 
-Hello {},My Name is  {}!. 
+Hello {},My Name is {} !. 
 
 I'm created To Set Filters only. 
 """
