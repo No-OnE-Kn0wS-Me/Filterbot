80cba6b4fcc7392f3f8d7faef7644d228bbc8fd3
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-01 10:18:12 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -11,12 +11,13 @@ class Config(object):
 
     # REQUIRED
     API_KEY = "YOUR KEY HERE"
-    OWNER_ID = "683538773"  # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "ViruZs"
+    OWNER_ID = "861055237"  # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "No_OnE_Kn0wS_Me"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
+    START_PHOTTO = "https://telegra.ph/file/7703ef03a791f158e28db.jpg" # Any img Url To shown In the Start Menu
     LOAD = []
     NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
