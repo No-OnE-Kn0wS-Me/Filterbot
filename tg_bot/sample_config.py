d548db32eb9730a5d3e77b0b6c224a134a5963a9
Ｈｉｔｓｕｋａ（芋ひュ） <61979182+No-OnE-Kn0wS-Me@users.noreply.github.com>
2020-08-01 12:01:29 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -17,7 +17,7 @@ class Config(object):
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
     MESSAGE_DUMP = None  # needed to make sure 'save from' messages persist
-    START_PHOTTO = "https://telegra.ph/file/7703ef03a791f158e28db.jpg" # Any img Url To shown In the Start Menu
+    START_PHOTTO = 'https://telegra.ph/file/7703ef03a791f158e28db.jpg' # Any img Url To shown In the Start Menu
     LOAD = []
     NO_LOAD = ['translation', 'rss']
     WEBHOOK = False
